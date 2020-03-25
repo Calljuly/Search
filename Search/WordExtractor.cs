5ab1448e6c9cc50c55a8eaecdcb007df7f6be1ed
patam123 <54626925+patam123@users.noreply.github.com>
2020-03-25 15:31:06 +0100

    started word extraction method

--- Search/WordExtractor.cs
+++ Search/WordExtractor.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace Search
 {
@@ -7,8 +9,17 @@ namespace Search
     {
         public List<Word> ExtractWords(string text, string filePath)
         {
+            var regexdText = Regex.Replace(text, "[.,{}¤#\"\'*:;<>|?=!_&/\\n\\r\\e\\t]", string.Empty);
+            var splittedText = regexdText.Split(' ');
             var wordList = new List<Word>();
+            foreach (var word in splittedText)
+            {
+                wordList.Add(new Word(word, filePath));
+            }
             return wordList;
+            //var regex = new Regex("[\\s\\W\\n\\t]");
+            //var test = text.Where(char.IsPunctuation).Distinct().ToArray();
+            //var words = text.Split().Select(x => x.Trim(test));
         }
     }
 }

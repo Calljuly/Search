7be83738ffcd40fc665c5b7ee8da20b8fe9c8fb4
Julia Call <54210280+Calljuly@users.noreply.github.com>
2020-03-25 12:48:10 +0100

    add

---
+++ Search/Soring.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Search
+{
+    class Soring
+    {
+        List<Word> listOfWord = new List<Word>();
+
+        public List<Word> sortAllWords(List<Word> list)
+        {
+            return list;
+        }
+
+    }
+}
---
+++ Search/Word.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Search
+{
+    class Word
+    {
+        public string word;
+        public string file;
+        public Word(string theWord, string theFile)
+        {
+            word = theWord;
+            file = theFile;
+        }
+    }
+}

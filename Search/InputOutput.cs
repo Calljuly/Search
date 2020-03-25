eb9be0f2a82839a6c0b87b47d7873b0b55b1566b
Yung Chi Timmy Wong <97yunwon@mk.se>
2020-03-25 14:40:38 +0100

    Added InputOutput class and IOTest

---
+++ Search/InputOutput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Search
+{
+    public class IO
+    {
+        public static string ReadFile()
+        {
+            return "";
+        }
+
+        public static void SaveFile(string file)
+        {
+
+        }
+    }
+}
---
+++ TestChamber/IOTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Search;
+
+namespace TestChamber
+{
+    public class IOTest
+    {
+        [Test]
+        public void IORead()
+        {
+            Assert.AreEqual("", IO.ReadFile());
+        }
+
+        [Test]
+        public void IOSave()
+        {
+            Assert.Pass();
+        }
+
+    }
+}

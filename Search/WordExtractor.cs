7147070028eade7f3592c51d93ddbb38120b8a20
patam123 <54626925+patam123@users.noreply.github.com>
2020-03-25 13:54:47 +0100

    added wordextractor as class, added test for extracting words

---
+++ Search/WordExtractor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Search
+{
+    public class WordExtractor 
+    {
+        public List<Word> ExtractWords(string text, string filePath)
+        {
+            var wordList = new List<Word>();
+            return wordList;
+        }
+    }
+}

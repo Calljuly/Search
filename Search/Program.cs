42e5b86e1d242e89c4e2ee1985b49440b0598e44
Yung Chi Timmy Wong <97yunwon@mk.se>
2020-03-31 08:20:27 +0200

    added using

--- Search/Program.cs
+++ Search/Program.cs
@@ -6,8 +6,42 @@ namespace Search
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            
+            List<string> filesLoaded = new List<string>();
+
+            while (true)
+            {
+                Console.WriteLine("1:Read file. 2:Search word. 3:Save file. 0:Exit. ");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("File Location exp \"C\\User\\... \"");
+                        Console.ReadLine();
+                        //read file method
+                        break;
+                    case "2":
+                        if (filesLoaded.Count > 0)
+                        {
+                            Console.Write("Type the word you want to search: ");
+                            Console.ReadLine();
+                            //sort and find method
+                        }
+                        else
+                        {
+                            Console.WriteLine("No file read yet");
+                        }
+                        break;
+                    case "3":
+                        //save file method
+                        break;
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input pleas try again.");
+                        break;
+                }
+            }
+
         }
     }
 }

a5edfe6136f516593291e0c8f97795b33884f649
Julia Call <54210280+Calljuly@users.noreply.github.com>
2020-04-07 08:45:06 +0200

    add
    more unittests for sort

--- TestChamber/SortingTests.cs
+++ TestChamber/SortingTests.cs
@@ -10,7 +10,7 @@ namespace TestChamber
         public List<Word> sut;
 
         [Test]
-        public void ifListGetSorted()
+        public void IfListGetSorted()
         {
             sut = new List<Word>();
             sut.Add(new Word("Maja", "TextFile"));
@@ -21,7 +21,7 @@ namespace TestChamber
         }
 
         [Test]
-        public void ifListGetSortedMoreWords()
+        public void IfListGetSortedMoreWords()
         {
             sut = new List<Word>();
             sut.Add(new Word("Hulda", "TextFile"));
@@ -36,7 +36,59 @@ namespace TestChamber
             Assert.AreEqual("Brygga", sut[0].Value);
         }
         [Test]
-        public void ifListGetSortedWithSimilarWords()
+        public void IfListGetSortedCheckLastWord()
+        {
+            sut = new List<Word>();
+            sut.Add(new Word("Hulda", "TextFile"));
+            sut.Add(new Word("Maja", "TextFile"));
+            sut.Add(new Word("Sten", "TextFile"));
+            sut.Add(new Word("Brygga", "TextFile"));
+            sut.Add(new Word("Kanna", "TextFile"));
+            sut.Add(new Word("Lama", "TextFile"));
+
+            Engine.QuickSort(sut, 0, 5);
+
+            Assert.AreEqual("Sten", sut[5].Value);
+        }
+        [Test]
+        public void IfListGetSortedCheckWordInMiddle()
+        {
+            sut = new List<Word>();
+            sut.Add(new Word("Hulda", "TextFile"));
+            sut.Add(new Word("Maja", "TextFile"));
+            sut.Add(new Word("Sten", "TextFile"));
+            sut.Add(new Word("Brygga", "TextFile"));
+            sut.Add(new Word("Kanna", "TextFile"));
+            sut.Add(new Word("Lama", "TextFile"));
+
+            Engine.QuickSort(sut, 0, 5);
+
+            Assert.AreEqual("Lama", sut[3].Value);
+        }
+
+        [Test]
+        public void IfASortedListSendsInNoChange()
+        {
+            sut = new List<Word>();
+            sut.Add(new Word("Alban", "TextFile"));
+            sut.Add(new Word("Bengt", "TextFile"));
+            sut.Add(new Word("Cilla", "TextFile"));
+            sut.Add(new Word("David", "TextFile"));
+            sut.Add(new Word("Emil", "TextFile"));
+            sut.Add(new Word("Felicia", "TextFile"));
+
+            Engine.QuickSort(sut, 0, 5);
+
+            Assert.AreEqual("Alban", sut[0].Value);
+            Assert.AreEqual("Bengt", sut[1].Value);
+            Assert.AreEqual("Cilla", sut[2].Value);
+            Assert.AreEqual("David", sut[3].Value);
+            Assert.AreEqual("Emil", sut[4].Value);
+            Assert.AreEqual("Felicia", sut[5].Value);
+
+        }
+        [Test]
+        public void IfListGetSortedWithSimilarWords()
         {
             sut = new List<Word>();
 
